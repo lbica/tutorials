93436fa60a4989d9e570263fd4a6948ddcdd6416
lbica <laurentiubica@yahoo.com>
2021-04-10 18:43:40 +0200

    add the last code for spark2 tutorial

--- LowestRatedMovieSpark2.py
+++ LowestRatedMovieSpark2.py
@@ -1,6 +1,11 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import Row
 from pyspark.sql import functions
+
+
+def loadMovieNames():
+    movieNames = {}
+
     with open('u.item') as f:
         for line in f:
             fields = line.split('|')
@@ -10,7 +15,7 @@ from pyspark.sql import functions
 
 def parseInput(line):
     fields = line.split()
-    return Row(movieId=int(fields[1]), rating= float(fields[2]))
+    return Row(movieID=int(fields[1]), rating=float(fields[2]))
 
 
 if __name__ == '__main__':
@@ -23,6 +28,8 @@ if __name__ == '__main__':
     # Load up the raw u.data file
     lines = sparkSession.sparkContext.textFile('hdfs:///data/ml-100k/u.data')
 
+    # lines = sparkSession.sparkContext.textFile('data/ml-100k/u.data')
+
     # Convert to a RDD of Row objects with (movieId, rating)
     movies = lines.map(parseInput)
 
@@ -33,14 +40,26 @@ if __name__ == '__main__':
     # ratingTotalsAndCount = movieRatings.reduceByKey(lambda movie1, movie2: (movie1[0] + movie2[0], movie1[1] + movie2[1]))
 
     # Map to (movieID, averageRating)
-    averageRatings = movieDataset.groupBy(lambda a: functions.mean(a))
+    averageRatings = movieDataset.groupBy("movieID").avg("rating") \
+        .withColumnRenamed("avg(rating)", "avg_rating")
 
-    # Sort by Average Ratings
-    # sortedMovies = averageRatings.sortBy(lambda x: x[1])
-    #
-    # # Take the Top 10 results
-    # results = sortedMovies.take(10)
-    #
-    # # Print them out
-    # for result in results:
-    #     print(movieNames[result[0]], result[1])
+    # Computes count of ratings for each movieID: (movieID, count)
+    counts = movieDataset.groupBy("movieID").count()
+
+    # Join the two together ( We now have movieID, avg(rating) and count columns
+
+    # define condition for joining dataframes:
+    cond = [averageRatings.movieID == counts.movieID]
+
+    averagesAndCounts = averageRatings.join(averageRatings, cond)
+
+    # Pull the top 10 results
+
+    topTen = averagesAndCounts.orderBy("avg_rating").take(20)
+
+    # # Print them out: convert movieID's to name as we go
+    for movie in topTen:
+        print(movieNames[movie[0]], movie[1], movie[2])
+
+    # Stop the spark session
+    sparkSession.stop()
